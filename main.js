4c1d2393870ad60fad99da841f44f48dceca74b4
FinDataMan <arturo.villagomezg@gmail.com>
2024-07-14 14:13:45 -0600

    create pool messages

---
+++ main.js
@@ -0,0 +1,42 @@
+//Type of random messages: LifeAdvice
+
+//Message Pool Categories:
+//Motivation and Inspiration
+const motivation1 = `"Believe in yourself, and you're halfway there."`;
+const motivation2 = `"The only limit is the one you impose on yourself."`;
+const motivation3 =  `"Every day is a new beginning. Embrace it with hope."`;
+
+//Mindfulness and Gratitude
+const mindfullness1 =  `"Focus on the journey, not just the destination."`;
+const mindfullness2 =  `"Be grateful for what you have; you'll end up having more."`;
+const mindfullness3 =  `"Find peace within yourself."`;
+
+//Relationships and Love
+const relationships1 =  `"Communication is the key to any relationship."`;
+const relationships2 =  `"Love deeply and unconditionally."`;
+const relationships3 =  `"Choose your friends wisely."`;
+
+//Personal Growth and Development
+const personalGrowth1 =  `"Step outside your comfort zone. That's where the magic happens."`;
+const personalGrowth2 =  `"Never stop learning."`;
+const personalGrowth3 =  `"Embrace change as an opportunity for growth."`;
+
+//Overcoming Challenges
+const overcomingChallenges1 =  `"Every challenge is an opportunity to grow stronger."`;
+const overcomingChallenges2 =  `"Don't let setbacks define you; let them refine you."`;
+const overcomingChallenges3 =  `"Perseverance is key to overcoming obstacles."`;
+
+//Happiness and Fulfillment
+const Happiness1 =  `"Happiness is not a destination; it's a journey."`;
+const Happiness2 =  `"Find joy in the little things."`;
+const Happiness3 =  `"Pursue your passions with enthusiasm."`;
+
+//Time Management and Productivity
+const productivity1 =  `"Prioritize tasks and focus on what matters."`;
+const productivity2 =  `"Take breaks to recharge."`;
+const productivity3 =  `"Time management is self-management."`;
+
+//Health and Wellness
+const health1 =  `"Nourish your body and mind."`;
+const health2 =  `"Sleep is essential for overall well-being."`;
+const health3 =  `"Find a physical activity you enjoy."`;
